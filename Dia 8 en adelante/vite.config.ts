5e873ba266c408fb1142cc26a0ea69ff119cf7c4
marvin <mcanastuj1999@gmail.com>
2026-02-05 16:04:14 -0600

    doing changes to vite file

--- Dia 8 en adelante/vite.config.ts
+++ Dia 8 en adelante/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  
+  build:{
+    rollupOptions:{
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dia9S: resolve(__dirname, 'pages/dia9_summary.html'),
+        dia9e: resolve(__dirname, 'pages/dia9_ejecicio.html'),
+      }
+    }
+  }
 })
